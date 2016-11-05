eab15884b492cc7e3a28f39d2bb6f8ca6f519fa5
Ttommeke <tomvalkeneers@hotmail.com>
2016-11-05 13:46:31 +0100

    camera movement gradualy towards point

--- js/base.js
+++ js/base.js
@@ -1,28 +1,34 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight-4);
 document.body.appendChild( renderer.domElement );
 var TimeClock = new THREE.Clock();
+var fpsCounter = new Stats();
+fpsCounter.showPanel(0);
+document.body.appendChild( fpsCounter.dom );
 
 var render = function() {
+	fpsCounter.begin();
 	var deltaTime = TimeClock.getDelta();
-	requestAnimationFrame( render );
 
 	if (Player.player != undefined) {
 		Player.executeKeys(deltaTime);
+		Camera.moveCamera(Camera.camera, Player.player.position, deltaTime, Camera.speed);
 	}
 
-	renderer.render( scene, camera );
+	renderer.render( scene, Camera.camera );
+	fpsCounter.end();
+
+	requestAnimationFrame( render );
 }
 
 $.getJSON("map.json", function(data) {
 	var emap = Map.generateMapFromNumberMap(data.map);
 	Map.loadMapInScene(emap, scene);
 
-	Utils.setXYZ(camera.position, data.cameras[0].position);
-	Utils.setXYZ(camera.rotation, data.cameras[0].rotation);
+	Camera.speed = data.cameraSpeed;
+	Camera.setCameraPositionAndRotation(Camera.camera, data.cameras[0]);
 
 	data.lights.forEach(function(light) {
 		scene.add(Light.generateLight(light));
---
+++ js/camera.js
@@ -0,0 +1,19 @@
+
+var Camera = {
+    "camera": new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ),
+    "speed": 4
+};
+
+Camera.moveCamera = function(camera, positionToMoveTo, deltaTime, speed) {
+    var difference = new THREE.Vector3(0,0,0);
+    difference.subVectors(positionToMoveTo, camera.position);
+    difference.y = 0;
+
+    camera.position.addScaledVector(difference, speed * deltaTime);
+
+}
+
+Camera.setCameraPositionAndRotation = function(cameraToSet, cameraJson) {
+    Utils.setXYZ(cameraToSet.position, cameraJson.position);
+	Utils.setXYZ(cameraToSet.rotation, cameraJson.rotation);
+}
